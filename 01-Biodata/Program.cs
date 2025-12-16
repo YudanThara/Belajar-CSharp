01a485fba2fb37c7b239b290c3f11c5625e02d86
Yudan <yudanprihantara98@gmail.com>
2025-12-16 21:09:45 +0700

    Upload biodata pertama

---
+++ 01-Biodata/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Belajar_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("=== BIODATA MAHASISWA ===");
+            Console.WriteLine("Nama   : Yudan Prihantara");
+            Console.WriteLine("Prodi  : Teknik Informatika");
+            Console.WriteLine("Hobi   : Gacha (WuWa, HSR), Novel");
+
+            Console.WriteLine("\nTekan sembarang tombol untuk keluar...");
+            Console.ReadKey(); 
+        }
+    }
+}
