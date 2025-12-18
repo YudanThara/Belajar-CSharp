7af7474362c081d7e3075510e3b5cbaa7e7e3030
Yudan <yudanprihantara98@gmail.com>
2025-12-18 21:54:32 +0700

    Menambahkan latihan oop encapsulation

---
+++ 11-OOP-Encapsulation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Belajar_CSharp 
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+        Mahasiswa mhs1 = new Mahasiswa();
+
+        mhs1.SetNama("Yudan Prihantara");
+        mhs1.SetUmur(19);
+
+        Console.WriteLine("Data Mahasiswa:");
+        Console.WriteLine("Nama : " + mhs1.GetNama());
+        Console.WriteLine("Umur : " + mhs1.GetUmur());
+
+        Console.ReadLine();
+        }
+    }
+}
